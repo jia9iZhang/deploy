1a45135ab03dcd212c988866225a211864d4a8a5
jiaqi.zhang <154269564@qq.com>
2022-03-15 23:08:37 +0800

    实现文件上传至Minio和列出桶中文件

--- minio/src/main/java/com/qi/controller/MinioController.java
+++ minio/src/main/java/com/qi/controller/MinioController.java
@@ -1,11 +1,9 @@
 package com.qi.controller;
 
 import com.qi.entriy.Result;
-import io.minio.BucketExistsArgs;
-import io.minio.MakeBucketArgs;
-import io.minio.MinioClient;
-import io.minio.UploadObjectArgs;
+import io.minio.*;
 import io.minio.errors.*;
+import io.minio.messages.Item;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,8 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 /**
@@ -31,22 +32,61 @@ public class MinioController {
     @Autowired
     Result result;
 
+    /**
+     * 测试
+     *
+     * @return
+     */
     @GetMapping("/hello")
     public String hello() {
         return "hello";
     }
 
-    @PostMapping("/list")
-    public List<Object> list() {
-        return null;
+    /**
+     * 获取桶中文件列表
+     *
+     * @return
+     * @throws ServerException
+     * @throws InsufficientDataException
+     * @throws ErrorResponseException
+     * @throws IOException
+     * @throws NoSuchAlgorithmException
+     * @throws InvalidKeyException
+     * @throws InvalidResponseException
+     * @throws XmlParserException
+     * @throws InternalException
+     */
+    @GetMapping("/list")
+    public ArrayList<String> list() throws ServerException, InsufficientDataException, ErrorResponseException, IOException, NoSuchAlgorithmException, InvalidKeyException, InvalidResponseException, XmlParserException, InternalException {
+        Iterable<io.minio.Result<Item>> listObjects = minioClient.listObjects(ListObjectsArgs.builder().bucket(MINIO_BUCKET).build());
+        Iterator<io.minio.Result<Item>> iterator = listObjects.iterator();
+        ArrayList<String> itemList = new ArrayList<>();
+        while (iterator.hasNext()) {
+            Item item = iterator.next().get();
+            itemList.add(item.objectName());
+        }
+        return itemList;
     }
 
+    /**
+     * 上传多文件至Minio
+     *
+     * @param multipartFiles
+     * @return
+     * @throws ServerException
+     * @throws InsufficientDataException
+     * @throws ErrorResponseException
+     * @throws IOException
+     * @throws NoSuchAlgorithmException
+     * @throws InvalidKeyException
+     * @throws InvalidResponseException
+     * @throws XmlParserException
+     * @throws InternalException
+     */
     @PostMapping("/upload")
     public String upload(@RequestParam(name = "file") MultipartFile[] multipartFiles) throws ServerException, InsufficientDataException, ErrorResponseException, IOException, NoSuchAlgorithmException, InvalidKeyException, InvalidResponseException, XmlParserException, InternalException {
 
         if (multipartFiles == null || multipartFiles.length == 0) {
-            result.setCode(500);
-            result.setMessage("上传文件不能为空");
             return "上传文件不能为空";
         }
 
@@ -57,23 +97,29 @@ public class MinioController {
         }
 
         for (MultipartFile file : multipartFiles) {
-            //
+            //遍历文件数组
             String originalFilename = file.getOriginalFilename();
+            try (InputStream in = file.getInputStream()) {
+                minioClient.putObject(PutObjectArgs.builder().bucket(MINIO_BUCKET).object(originalFilename).stream(in, in.available(), -1).build());
+            }
 
-            minioClient.uploadObject(UploadObjectArgs.builder()
-                    .bucket(MINIO_BUCKET)
-                    .object(originalFilename)
-                    //Win 上传成功
-                    .filename("C:\\Users\\Administrator\\Desktop\\JVM.png")
-                    //mac 因为权限问题 无法访问磁盘
+//            minioClient.uploadObject(UploadObjectArgs.builder()
+//                    .bucket(MINIO_BUCKET)
+//                    .object(originalFilename)
+//                    //Win 上传成功
+////                    .filename("C:\\Users\\Administrator\\Desktop\\JVM.png")
+//                    //mac 因为权限问题 无法访问磁盘
 //                    .filename("/Users/jiaqi.zhang/Downloads/dahai.jpeg")
-                    .build());
+//                    .build());
         }
-        result.setCode(200);
-        result.setMessage("上传成功");
         return "上传成功";
     }
 
+    /**
+     * 从桶中下载文件
+     *
+     * @return
+     */
     @PostMapping("/download")
     public List<Object> download() {
         return null;
