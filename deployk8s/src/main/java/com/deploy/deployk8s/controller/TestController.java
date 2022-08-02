a278cd20aaedb09c9fedd150a9b5f88a78d50507
jiaqi.zhang <154269564@qq.com>
2022-08-02 21:07:20 +0800

    [init] project

---
+++ deployk8s/src/main/java/com/deploy/deployk8s/controller/TestController.java
@@ -0,0 +1,17 @@
+package com.deploy.deployk8s.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author jiaqi.zhang
+ * @version 1.0
+ * @date 2022/8/2 20:13
+ */
+@RestController
+public class TestController {
+    @GetMapping("/test")
+    public String test(){
+        return "welcome...";
+    }
+}
